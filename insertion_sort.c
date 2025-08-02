45fb8abd37fae1ab46df9ccc3e056961b5c78031
Thomas Mauro <thom_mauro@outlook.com>
2025-08-02 13:09:20 -0300

    ordenação por insertion sort

---
+++ insertion_sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define N 6
+
+void insertion_sort(int v[])
+{
+    for (int j=1; j < N; j++) {
+        int key = v[j];
+        int i = j - 1;
+
+        while (i >= 0 && v[i] > key) {
+            v[i + 1] = v[i];
+            i--;
+        }
+
+        v[i + 1] = key;
+    }
+
+    return;
+}
+
+void print_array(int v[])
+{
+    for (int j=0; j<N; j++) {
+        printf("%d ", v[j]);
+    }
+    printf("\n");
+
+    return;
+}
+
+int main()
+{
+    int v[N] = {5, 3, 83, 32, 13, 1};
+    int v2[N] = {5, 3, 83, 32, 13, 1};
+
+    printf("Initial array: ");
+    print_array(v);
+
+    insertion_sort(v);
+
+    printf("Sorted array: ");
+    print_array(v);
+
+}
