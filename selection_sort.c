902289f579cc2584e1208d0d890fe7a5a4cd8589
Thomas Mauro <thom_mauro@outlook.com>
2025-08-04 22:00:32 -0300

    adiciona selection_sort

---
+++ selection_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void print_array(int v[], int len)
+{
+    for (int j=0; j<len; j++) {
+        printf("%d ", v[j]);
+    }
+    printf("\n");
+
+    return;
+}
+
+void selection_sort(int v[], int len)
+{
+    for (int j=0; j<len; j++) {
+
+        int smallest_index = j;
+
+        for (int k=j; k<len; k++) {
+
+            if (v[k] < v[smallest_index]) smallest_index = k;
+
+        }
+
+        int tmp = v[smallest_index];
+        v[smallest_index] = v[j];
+        v[j] = tmp;
+
+    }
+}
+
+int main()
+{
+
+    int v[3] = {5, 4, 3};
+    selection_sort(v, 3);
+    print_array(v, 3);
+
+    return 0;
+}
